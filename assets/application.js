96d57bfb994d7aa9e3c4592514202063b64be167
Diego Alba <diegoalba15@icloud.com>
2021-09-16 23:08:17 -0400

    initial setup and vue installation

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,11 @@
-// Put your application javascript here
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    //dom is fully loaded, but maybe waiting on images & css files
+    (function log(){
+        const searchIcon = document.querySelector('[search-icon]');
+        searchIcon.addEventListener('click', () => {
+            console.log('search');
+        });
+    })();
+});
